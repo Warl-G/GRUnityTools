f3a66820d6f07ed2215aa631f956414ddfe4b7ab
Warl-G <>
2020-04-10 21:21:31 +0800

    本地化工具重构

---
+++ Runtime/Localization/LocalizationComponent.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GRTools.Localization
+{
+    [Serializable]
+    public class LocalizationComponentItem
+    {
+        [Tooltip("本地化控件")] [SerializeField] internal Component component;
+        [Tooltip("本地化键")] public string localizationKey;
+        [Tooltip("本地化默认值（文本，图片名称）")] public string defaultValue;
+        [Tooltip("是否设置图片原始尺寸")] public bool setNativeSize;
+        
+    }
+    
+    public class LocalizationComponent : MonoBehaviour
+    {
+        private SystemLanguage _currentLanguage;
+
+        public LocalizationComponentItem[] items;
+        private void Start()
+        {
+            if (LocalizationManager.Singleton != null)
+            {
+                LocalizationChanged(LocalizationManager.Singleton.CurrentLocalizationFile);
+            }
+            LocalizationManager.LocalizationChangeEvent += LocalizationChanged;
+        }
+
+        private void OnDestroy()
+        {
+            LocalizationManager.LocalizationChangeEvent -= LocalizationChanged;
+        }
+
+        private void LocalizationChanged(LocalizationFile localizationFile)
+        {
+            _currentLanguage = localizationFile.Type;
+            foreach (var item in items)
+            {
+                if (!string.IsNullOrEmpty(item.localizationKey) && item.component != null)
+                {
+                    string value = LocalizationManager.Singleton.GetLocalizedText(item.localizationKey);
+                    if (value == null)
+                    {
+                        value = item.defaultValue;
+                    }
+
+                    if (item.component.GetType() == typeof(Text))
+                    {
+                        ((Text)item.component).text = value;
+                    }
+                    else
+                    {
+                        Image imageComponent = null;
+                        SpriteRenderer spriteRenderer = null;
+
+                        if (item.component.GetType() == typeof(Image))
+                        {
+                            imageComponent = (Image) item.component;
+                        }
+
+                        if (item.component is SpriteRenderer)
+                        {
+                            spriteRenderer = (SpriteRenderer) item.component;
+                        }
+
+                        if (imageComponent != null || spriteRenderer != null)
+                        {
+                            //替换图片
+                            StartCoroutine(LocalizationManager.Singleton.GetLocalizedSpriteByNameAsync(value, item.defaultValue,
+                                delegate(Sprite sprite)
+                                {
+                                    if (sprite != null)
+                                    {
+                                        if (imageComponent != null)
+                                        {
+                                            Image img = ((Image) item.component);
+                                            img.sprite = sprite;
+                                            if (item.setNativeSize)
+                                            {
+                                                img.SetNativeSize();
+                                            }
+                                        }
+
+                                        if (spriteRenderer != null)
+                                        {
+                                            spriteRenderer.sprite = sprite;
+                                        }
+                                    }
+                                })
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Runtime/Localization/LocalizationParser.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace GRTools.Localization
+{
+    public class LocalizationParser
+    {
+        public enum LocalizationFileType
+        {
+            Txt,
+            Csv,
+            Asset
+        }
+
+        public Dictionary<string, string> ParseFile(string filePath, LocalizationFileType type = LocalizationFileType.Txt)
+        {
+            if (type == LocalizationFileType.Txt)
+            {
+                return ParseTxtFile(filePath);
+            }
+
+            return null;
+        }
+        
+        private Dictionary<string, string> ParseTxtFile(string filePath)
+        {
+            TextAsset asset = Resources.Load<TextAsset>(filePath);
+            if (asset == null)
+            {
+                Debug.LogError("no localizefile " + filePath);
+            }
+            else
+            {
+                Dictionary<string, string> dict = ParseTxt(asset.text);
+                Resources.UnloadAsset(asset);
+                return dict;
+            }
+
+            return null;
+        }
+    
+        public Dictionary<string, string> ParseTxt(string txt)
+        {
+            if (string.IsNullOrEmpty(txt))
+            {
+                return null;
+            }
+            string[] lines = txt.Split('\n');
+            Dictionary<string, string> localDict = new Dictionary<string, string>();
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    string[] keyAndValue = line.Split(new[] {'='}, 2);
+                    if (keyAndValue.Length == 2)
+                    {
+                        string value = Regex.Unescape(keyAndValue[1]);
+                        localDict.Add(keyAndValue[0], value);
+                    }
+                }
+            }
+
+            return localDict;
+        }
+
+    }
+}
+
