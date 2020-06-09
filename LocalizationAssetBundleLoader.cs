9bd5917b47e2ee70cd50878fa9e932bcab3a6dd0
Warl-G <>
2020-06-09 23:48:34 +0800

    拆分 GRTools

---
+++ LocalizationAssetBundleLoader.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace GRTools.Localization
+{
+    public class LocalizationAssetBundleLoader : LocalizationLoader
+    {
+        /// <summary>
+        /// 公共语言资源Bundle名称
+        /// </summary>
+        public string CommonAssetsPath = "Common";
+        
+        /// <summary>
+        /// 是否卸载上一个本地化 AssetBundle
+        /// </summary>
+        public bool UnloadLastLocalizationBundle = true;
+
+        private AssetBundle _assetBundle;
+        private AssetBundle _commonBundle;
+
+        public override void LoadAllFileListAsync(Action<LocalizationFile[]> complete)
+        {
+            if (complete != null)
+            {
+                string bundlePath = Path.Combine(Application.streamingAssetsPath, FilesPath, FilesPath);
+
+                var loadrequest = AssetBundle.LoadFromFileAsync(bundlePath);
+                loadrequest.completed += operation =>
+                { 
+                    AssetBundleManifest manifest =
+                        loadrequest.assetBundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+                    var files = manifest.GetAllAssetBundles();
+
+                    List<LocalizationFile> fileList = new List<LocalizationFile>();
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        if (!files[i].Equals(CommonAssetsPath.ToLower()))
+                        {
+                            LocalizationFile data = new LocalizationFile(files[i]);
+                            fileList.Add(data);
+                        }
+                    }
+
+                    loadrequest.assetBundle.Unload(manifest);
+                    complete(fileList.ToArray());
+                };
+            }
+        }
+
+        public override void LoadAssetAsync<T>(string localizationFileName, string assetPath, bool defaultAsset,
+            Action<T> complete)
+        {
+            if (!string.IsNullOrEmpty(localizationFileName) && !string.IsNullOrEmpty(assetPath) && complete != null)
+            {
+                if (defaultAsset)
+                {
+                    LoadCommonBundle(assetPath, complete);
+                }
+                else
+                {
+                    LoadLocalizationBundle(localizationFileName, assetPath, complete);
+                }
+            }
+        }
+
+        private void LoadLocalizationBundle<T>(string localizationFileName, string assetPath, Action<T> complete)
+            where T : UnityEngine.Object
+        {
+            void LoadAsset()
+            {
+                if (_assetBundle != null)
+                {
+                    var request = _assetBundle.LoadAssetAsync<T>(assetPath);
+                    request.completed += asyncOperation => { complete(request.asset as T); };
+                }
+            }
+
+            AssetBundleCreateRequest loadrequest = null;
+            if (_assetBundle == null)
+            {
+                loadrequest = AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, FilesPath,
+                    localizationFileName));
+            }
+            else if (!_assetBundle.name.Equals(localizationFileName))
+            {
+                //加载新语言包，则卸载旧语言包，可能导致部分资源缺失
+                if (UnloadLastLocalizationBundle)
+                {
+                    _assetBundle.Unload(true);
+                    _assetBundle = null;
+                }
+                
+                loadrequest = AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, FilesPath,
+                    localizationFileName));
+            }
+
+            if (loadrequest != null)
+            {
+                loadrequest.completed += operation =>
+                {
+                    _assetBundle = loadrequest.assetBundle;
+                    LoadAsset();
+                };
+            }
+            else
+            {
+                LoadAsset();
+            }
+        }
+
+        private void LoadCommonBundle<T>(string assetPath, Action<T> complete) where T : UnityEngine.Object
+        {
+            void LoadAsset()
+            {
+                if (_commonBundle != null)
+                {
+                    var request = _commonBundle.LoadAssetAsync<T>(assetPath);
+                    request.completed += asyncOperation => { complete(request.asset as T); };
+                }
+            }
+
+            if (_commonBundle == null)
+            {
+                var request =
+                    AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, FilesPath,
+                        CommonAssetsPath));
+                request.completed += operation =>
+                {
+                    _commonBundle = request.assetBundle;
+                    LoadAsset();
+                };
+            }
+            else
+            {
+                LoadAsset();
+            }
+        }
+
+#if UNITY_EDITOR
+
+        public static void BuildLocalizationAssets(string localizationFilePath, BuildTarget target)
+        {
+            string[] paths = Directory.GetDirectories(localizationFilePath);
+            AssetBundleBuild[] buildMap = new AssetBundleBuild[paths.Length];
+
+            for (int i = 0; i < paths.Length; i++)
+            {
+                string bundleName = Path.GetFileName(paths[i]);
+                buildMap[i].assetBundleName = bundleName;
+                var files = Directory.GetFiles(paths[i], "*", SearchOption.AllDirectories);
+
+                List<string> assets = new List<string>();
+                foreach (var file in files)
+                {
+                    if (!file.EndsWith(".meta"))
+                    {
+                        var filePath = file.Replace(Application.dataPath, "Assets");
+                        assets.Add(filePath);
+                    }
+                }
+
+                buildMap[i].assetNames = assets.ToArray();
+            }
+
+            if (paths.Length > 0)
+            {
+                var parent = Path.GetFileName(localizationFilePath);
+                
+                if (!AssetDatabase.IsValidFolder("Assets/StreamingAssets"))
+                {
+                    AssetDatabase.CreateFolder("Assets", "StreamingAssets");
+                }
+
+                if (!AssetDatabase.IsValidFolder("Assets/StreamingAssets/" + parent))
+                {
+                    AssetDatabase.CreateFolder("Assets/StreamingAssets", parent);
+                }
+                
+                
+                BuildPipeline.BuildAssetBundles("Assets/StreamingAssets/" + parent, buildMap, BuildAssetBundleOptions.ChunkBasedCompression, target);
+            }
+        }
+
+        [MenuItem("GRTools/Localization/Build AssetBundles")]
+        public static void BuildABsMenu()
+        {
+            string key = "LocalizationFilePath";
+            string localizationFilePath =
+                EditorPrefs.GetString(key, "");
+
+            void ShowDialog()
+            {
+                int index = EditorUtility.DisplayDialogComplex("LocalizationFilePath", localizationFilePath, "Build",
+                    "Cancel",
+                    "Select Folder");
+                if (index == 2)
+                {
+                    ShowFolderSelector();
+                }
+                else if (index == 0)
+                {
+                    BuildBundle();
+                }
+            }
+
+            void ShowFolderSelector()
+            {
+                localizationFilePath =
+                    EditorUtility.OpenFolderPanel("LocalizationFilePath", localizationFilePath, "Localization");
+                ShowDialog();
+            }
+
+            ShowDialog();
+
+            void BuildBundle()
+            {
+                if (!Directory.Exists(localizationFilePath))
+                {
+                    EditorUtility.DisplayDialog("", "Invalid path", "OK");
+                    ShowDialog();
+                    return;
+                }
+
+                EditorPrefs.SetString(key, localizationFilePath);
+
+                BuildLocalizationAssets(localizationFilePath, EditorUserBuildSettings.activeBuildTarget);
+            }
+        }
+#endif
+    }
+}
---
+++ LocalizationParser.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using GRTools.Utils;
+using Newtonsoft.Json;
+
+namespace GRTools.Localization
+{
+    /// <summary>
+    /// 可解析类型
+    /// </summary>
+    public enum LocalizationFileType
+    {
+        Txt,
+        Csv,
+        Json
+    }
+    public interface ILocalizationParser
+    {
+        /// <summary>
+        /// 文本解析
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        Dictionary<string, string> Parse(string text);
+    }
+    public class LocalizationDefaultParser : ILocalizationParser
+    {
+        public LocalizationFileType parseType = LocalizationFileType.Csv;
+
+        public LocalizationDefaultParser(LocalizationFileType type = LocalizationFileType.Csv)
+        {
+            parseType = type;
+        }
+        /// <summary>
+        /// 解析本地化文本
+        /// </summary>
+        /// <param name="text">本地化文本</param>
+        /// <returns></returns>
+        public Dictionary<string, string> Parse(string text)
+        {
+            if (parseType == LocalizationFileType.Csv)
+            {
+                return ParseCsv(text);
+            }
+            
+            if (parseType == LocalizationFileType.Txt)
+            {
+                return ParseTxt(text);
+            }
+
+            if (parseType == LocalizationFileType.Json)
+            {
+                return ParseJson(text);
+            }
+
+            return null;
+        }
+    
+        /// <summary>
+        /// 解析本地化 txt 文本
+        /// </summary>
+        /// <param name="txt"></param>
+        /// <returns></returns>
+        public Dictionary<string, string> ParseTxt(string txt)
+        {
+            if (string.IsNullOrEmpty(txt))
+            {
+                return null;
+            }
+            string[] lines = txt.Split('\n');
+            Dictionary<string, string> localDict = new Dictionary<string, string>();
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    string[] keyAndValue = line.Split(new[] {'='}, 2);
+                    if (keyAndValue.Length == 2)
+                    {
+                        string value = Regex.Unescape(keyAndValue[1]);
+                        localDict.Add(keyAndValue[0], value);
+                    }
+                }
+            }
+
+            return localDict;
+        }
+
+        /// <summary>
+        /// 解析本地化 csv 文本
+        /// </summary>
+        /// <param name="csv"></param>
+        /// <returns></returns>
+        public Dictionary<string, string> ParseCsv(string csv)
+        {
+            if (string.IsNullOrEmpty(csv))
+            {
+                return null;
+            }
+            Dictionary<string, string> localDict = new Dictionary<string, string>();
+            var dict = CsvParser.ParseRowToDictionary(csv);
+            foreach (var key in dict.Keys)
+            {
+                var values = dict[key];
+                if (!string.IsNullOrEmpty(key) && values.Count > 0)
+                {
+                    localDict.Add(key, values[0].ToString());
+                }
+            }
+
+            return localDict;
+        }
+
+        /// <summary>
+        /// 解析本地化 json 文本
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public Dictionary<string, string> ParseJson(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+            {
+                return null;
+            }
+            Dictionary<string, string> localDict = new Dictionary<string, string>();
+            var dict = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            foreach (var key in dict.Keys)
+            {
+                string value = Regex.Unescape(dict[key].ToString());
+                localDict.Add(key, value);
+            }
+            
+            return localDict;
+        }
+    }
+}
+
