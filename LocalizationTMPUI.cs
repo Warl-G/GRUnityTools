f1854d1515c832f5a0ea35e7f55a2caae983c960
Warl-G <>
2021-05-02 19:30:36 +0800

    update localization util

---
+++ LocalizationTMPUI.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using TMPro;
+
+namespace GRTools.Localization
+{
+    public class LocalizationTMPUGUI : MonoBehaviour
+    {
+        [Tooltip("本地化键")] public string localizationKey;
+        [Tooltip("本地化默认文本")] public string defaultValue;
+        
+        [SerializeField] private TextMeshProUGUI text;
+
+        private void Start()
+        {
+            if (LocalizationManager.Singleton != null && LocalizationManager.Singleton.CurrentLocalizationInfo != null)
+            {
+                OnLocalizationChanged(LocalizationManager.Singleton.CurrentLocalizationInfo);
+            }
+            LocalizationManager.LocalizationChangeEvent += OnLocalizationChanged;
+        }
+        
+        private void OnDestroy()
+        {
+            LocalizationManager.LocalizationChangeEvent -= OnLocalizationChanged;
+        }
+
+        private void OnLocalizationChanged(LocalizationInfo localizationFile)
+        {
+            if (text == null)
+            {
+                text = GetComponent<TextMeshProUGUI>();
+            }
+            if (text != null)
+            {
+                string value = LocalizationManager.Singleton.GetLocalizedText(localizationKey, defaultValue);
+                text.text = value;
+            }
+        }
+    }
+}
